58638a0d341835d13d109e633c66c024b6105eb8
yeonghyeon <elitebrain@naver.com>
2020-09-28 20:31:34 +0900

    Updates

---
+++ precache-manifest.5d98dc7f288c5f9355e63b1a06af3b1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ee8b96334f1860b40fccfc79180d2f",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "bc628d1d76f6163be445",
+    "url": "/nwitter/static/css/main.c5a87bac.chunk.css"
+  },
+  {
+    "revision": "114ade84ac00906ed3f4",
+    "url": "/nwitter/static/js/2.34298e5e.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/nwitter/static/js/2.34298e5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc628d1d76f6163be445",
+    "url": "/nwitter/static/js/main.0191a8e9.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
